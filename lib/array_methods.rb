898ada6536b108deda459fb4d643efb9ae70b8e1
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-07-04 20:32:42 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,22 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+   counter = 0
+  while counter < array.length do
+ return array.index(value_to_find)
+ end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  counter = 0
+while counter < array.length do
+counter +=1
+return array.max
+end
 end
 
 def find_min_value(array)
-  # Add your solution here
+  counter = 0
+while counter < array.length do
+counter +=1
+return array.min
+end
 end
